bc82443068dd8fae216d8352324fef290d0214ea
unknown <adiguna.hakim@gmail.com>
2021-03-07 12:12:58 +0700

    add dev dependencies nodemon

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res, next) => {
-    res.json({
-        "message" : "welcome to homepage"
-    })
+    res.redirect('/home');
 });
+
+app.get('/home', (req, res, next) => {
+    res.send('welcome to home page');
+})
 
 module.exports = app;
